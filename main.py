8f3a330330f9d8786c67ebf6185655de964eecfa
Sanyam Karnavat <sanyam.karnavat5@gmail.com>
2026-08-11 15:14:34 +0530

    ---

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="KundliWheel API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://wheel-sable-seven.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
